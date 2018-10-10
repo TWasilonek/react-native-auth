aa3de74a8ee360fa1caf3ec9a80b01dee9b035ea
Tomasz Wasilonek <tomasz.wasilonek@gmail.com>
2018-10-10 14:11:26 +0800

    Added Forgot Password screen stub

--- screens/ForgotPassword.js
+++ screens/ForgotPassword.js
@@ -1,18 +1,58 @@
 import React from 'react';
-import { View } from 'react-native';
-import { Text } from 'react-native-elements';
+import PropTypes from 'prop-types';
+import { View, StyleSheet } from 'react-native';
+import { Button, Text} from 'react-native-elements';
+
+import ScreenContainer from '../components/ScreenContainer';
+import AppInput from '../components/AppInput';
 
 class ForgotPassword extends React.PureComponent {
+  state = {
+    email: '',
+  };
+
+  onChangeInput = (text, name) => {
+    this.setState({ [name]: text });
+  };
+
+  onSubmit = () => {
+    const { navigation } = this.props;
+    navigation.navigate('Login');
+  }
+
   render() {
+    const { email } = this.state;
     return (
-      <View>
-        <Text>ForgotPassword</Text>
-        <Text>ForgotPassword</Text>
-        <Text>ForgotPassword</Text>
-        <Text>ForgotPassword</Text>
-      </View>
+      <ScreenContainer>
+        <View style={styles.container}>
+          <Text>
+            We will send the instructions how you can reset your password to your email.
+          </Text>
+          <AppInput
+            label="Email"
+            onChangeInput={(value) => this.onChangeInput(value, 'email')}
+          />
+          <Button
+            title='SEND EMAIL'
+            onPress={this.onSubmit}
+            disabled={!email}
+          />
+        </View>
+      </ScreenContainer>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    width: '90%',
+    marginLeft: 'auto',
+    marginRight: 'auto'
+  },
+});
+
+ForgotPassword.propTypes = {
+  navigation: PropTypes.shape({}).isRequired
+};
 
 export default ForgotPassword;
